c22545f7c05f1651e1c24afe6708293e3f27a94d
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-03-02 23:04:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+/* module.exports = function towelSort (matrix) {
   return [];
 }
+*/ 
+
+module.exports = function towelSort (matrix) {
+
+  if (matrix === undefined) {
+    return [];
+  }
+
+  let arrLonly = []
+
+matrix.forEach(function(item, i, arr) {
+  if (i%2 == 0) {
+    arrLonly.push(item);
+  }  else {
+    arrLonly.push(item.reverse());
+  } 
+});
+
+  return arrLonly.flat(Infinity);
+
+}
+//
